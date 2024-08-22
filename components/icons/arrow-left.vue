ebc91ceec5c8cdbbe78e74ad920b20d23fbd90d1
Alex Liebrich <alexanderliebrich@web.de>
2024-08-22 11:32:14 +0200

    setview: nav btn icons

---
+++ components/icons/arrow-left.vue
@@ -0,0 +1,8 @@
+<template>
+<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+
+<path d="M19 12H5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12 19L5 12L12 5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+
+</svg>
+</template>
