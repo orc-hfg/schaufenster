4e5c12e28c605d28f6919d77daa9d5be3b2c8aa0
Alex Liebrich <alexanderliebrich@web.de>
2024-08-28 23:04:50 +0200

    year selector: new up down icons

--- components/icons/YearSelectorDown.vue
+++ components/icons/YearSelectorDown.vue
@@ -1,5 +1,9 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" width="54" height="48" viewBox="0 0 54 48" fill="none">
+<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+  <path d="M23.5 43L1 3L46 3L23.5 43Z" fill="#FF4D00"/>
+</svg>
+    <!--<svg xmlns="http://www.w3.org/2000/svg" width="54" height="48" viewBox="0 0 54 48" fill="none">
         <path d="M27 48L-3.4331e-06 -4.72083e-06L54 0L27 48Z" fill="#2C2C2C"/>
     </svg>
+    -->
 </template>
--- components/icons/YearSelectorUp.vue
+++ components/icons/YearSelectorUp.vue
@@ -1,7 +1,11 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" width="54" height="48" viewBox="0 0 54 48" fill="none">
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+    <path d="M23.5 5L46 45L0.999993 45L23.5 5Z" fill="#FF4D00"/>
+  </svg>
+
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" width="54" height="48" viewBox="0 0 54 48" fill="none">
   <g >
     <path d="M27 4.72083e-06L54 48L-8.39259e-06 48L27 4.72083e-06Z" fill="#2C2C2C"/>
   </g>
-</svg>
+</svg> -->
 </template>
