7f0ebf0b059d0053a9167b46256b668feaa4a26d
Alex Liebrich <alexanderliebrich@web.de>
2024-08-29 11:35:29 +0200

    setlist: btm bar fold icons: WIP

--- components/icons/BtmBarFoldMinus.vue
+++ components/icons/BtmBarFoldMinus.vue
@@ -1,6 +1,17 @@
 <template>
+<!-- <svg xmlns="http://www.w3.org/2000/svg" width="28" height="2" viewBox="0 0 28 2" fill="none">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M0 -1.22392e-06L28 0L28 2L-8.74228e-08 2L0 -1.22392e-06Z" fill="black"/>
+</svg> -->
     <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
-        <!-- <path d="M0 24C0 10.7452 10.7452 0 24 0C37.2548 0 48 10.7452 48 24C48 37.2548 37.2548 48 24 48C10.7452 48 0 37.2548 0 24Z" fill="#F3F2EF" fill-opacity="0.7"/> -->
         <path d="M38 24L10 24" stroke="black" stroke-width="2" stroke-linejoin="round"/>
     </svg>
 </template>
+<style scoped>
+svg {
+    position:absolute;
+    top: 2px; left: 0px;
+    /* fill: var(--btm-bar-main-button-fill, rgba(243, 242, 239, 0.70)); */
+    fill: var(--btm-bar-main-button-icon, #000);
+    stroke: var(--btm-bar-main-button-icon, #000);
+}
+</style>
--- components/icons/BtmBarFoldPlus.vue
+++ components/icons/BtmBarFoldPlus.vue
@@ -1,11 +1,18 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M13 13V0H15V13H28V15H15V28H13V15H0V13H13Z" fill="black"/>
+    </svg>
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
         <path d="M24 10V38" stroke="black" stroke-width="2" stroke-linejoin="round"/>
         <path d="M10 24H38" stroke="black" stroke-width="2" stroke-linejoin="round"/>
-    </svg>
+    </svg> -->
 </template>
 <style scoped>
 svg {
-    fill: var(--btm-bar-main-button-fill, rgba(243, 242, 239, 0.70));
+    position: absolute;
+    top: 12px; left: 12px;
+    /* fill: var(--btm-bar-main-button-fill, rgba(243, 242, 239, 0.70)); */
+    fill: var(--btm-bar-main-button-icon, #000);
+    stroke: var(--btm-bar-main-button-icon, #000);
 }
 </style>
