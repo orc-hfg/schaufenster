8d52fe9968bd86b8e183c77dbe1bca87b162673c
Alex Liebrich <alexanderliebrich@web.de>
2024-08-29 11:32:15 +0200

    setview: slider nav btn icons

---
+++ components/icons/SliderArrowLeft.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="28" viewBox="0 0 32 28" fill="none">
+        <g style="mix-blend-mode:difference">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M3.43461 15.0195L16.3492 26.5059L14.9007 28L0 14.7471L1.26878e-07 13.2529L14.9007 0L16.3492 1.4941L3.43461 12.9805H18.2279H32V15.0195L3.43461 15.0195Z" fill="white"/>
+        </g>
+    </svg>
+    </div>
+</template>
+<style scoped>
+.icon {
+    display: flex;
+width: var(--dimension-icon-size-L, 32px);
+height: var(--dimension-icon-size-L, 32px);
+padding: var(--margin-navbar-institution-logo-right, 10px);
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: var(--margin-navbar-institution-logo-right, 10px);
+}
+svg {
+    width: 32px;
+    height: 28px;
+    flex-shrink: 0;
+
+    fill: #FFF;
+    mix-blend-mode: difference;
+}
+</style>
---
+++ components/icons/SliderArrowRight.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="28" viewBox="0 0 32 28" fill="none">
+        <g style="mix-blend-mode:difference">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M17.5839 28L31.9999 14.7471L31.9999 13.253L17.5838 1.1074e-05L16.1825 1.49412L28.6981 13L1.31134e-06 13L1.3131e-06 15L28.6982 15L16.1825 26.5059L17.5839 28Z" fill="white"/>
+        </g>
+    </svg>
+    </div>
+</template>
+<style scoped>
+.icon {
+    display: flex;
+width: var(--dimension-icon-size-L, 32px);
+height: var(--dimension-icon-size-L, 32px);
+padding: var(--margin-navbar-institution-logo-right, 10px);
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: var(--margin-navbar-institution-logo-right, 10px);
+}
+svg {
+    width: 32px;
+    height: 28px;
+    flex-shrink: 0;
+
+    fill: #FFF;
+    mix-blend-mode: difference;
+}
+
+</style>
