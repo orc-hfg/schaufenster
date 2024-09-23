9bcdd813a92796565d442d788a1efe10f0cb74c3
Alex Liebrich <alexanderliebrich@web.de>
2024-09-23 10:36:53 +0200

    setview: audio background icon

---
+++ components/icons/AudioBack.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg width="120" height="196" viewBox="0 0 120 196" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g opacity="0.08">
+            <path d="M10 69L10 127" stroke="white" stroke-width="20" stroke-linecap="round"/>
+            <path d="M60 10L60 186" stroke="white" stroke-width="20" stroke-linecap="round"/>
+            <path d="M110 47L110 149" stroke="white" stroke-width="20" stroke-linecap="round"/>
+        </g>
+    </svg>
+
+</template>
+<style scoped>
+svg {
+    /* border: 20px solid transparent;
+    width: 102px;
+    height: 0px;
+    transform: rotate(90deg); */
+}
+path {
+
+}
+</style>
