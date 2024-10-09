e7387ccc9fccf6af9d1a564283de22f1a321749a
Alex Liebrich <alexanderliebrich@web.de>
2024-10-09 06:24:32 +0200

    setview: main slider: nav-btn: filter contrast(0)

--- components/icons/SliderArrowLeft.vue
+++ components/icons/SliderArrowLeft.vue
@@ -1,38 +1,43 @@
 <template>
-    <div class="icon">
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="28" viewBox="0 0 32 28" fill="none">
-        <!-- <g style="mix-blend-mode:difference"> -->
-        <g>
-            <path fill-rule="evenodd" 
-            clip-rule="evenodd" 
-            d="M3.43461 15.0195L16.3492 26.5059L14.9007 28L0 14.7471L1.26878e-07 13.2529L14.9007 0L16.3492 1.4941L3.43461 12.9805H18.2279H32V15.0195L3.43461 15.0195Z" 
+    <div class="slider_arrow_icon_left">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="28" viewBox="0 0 32 28" fill="none">
+            <g>
+                <path fill-rule="evenodd" 
+                clip-rule="evenodd" 
+                d="M3.43461 15.0195L16.3492 26.5059L14.9007 28L0 14.7471L1.26878e-07 13.2529L14.9007 0L16.3492 1.4941L3.43461 12.9805H18.2279H32V15.0195L3.43461 15.0195Z" 
                 fill="var(--Colors-text-primary, #2C2C2C)"/>
-        </g>
-    </svg>
+            </g>
+        </svg>
     </div>
 </template>
 <style scoped>
-.icon {
+.slider_arrow_icon_left {
     display: flex;
-width: var(--dimension-icon-size-L, 32px);
-height: var(--dimension-icon-size-L, 32px);
-padding: var(--margin-navbar-institution-logo-right, 10px);
-flex-direction: column;
-justify-content: center;
-align-items: center;
-gap: var(--margin-navbar-institution-logo-right, 10px);
+
+    width: var(--dimension-icon-size-L, 32px);
+    height: var(--dimension-icon-size-L, 32px);
+    padding: var(--margin-navbar-institution-logo-right, 10px);
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: var(--margin-navbar-institution-logo-right, 10px);
+    color: var(--Colors-text-primary, #2C2C2C);
+    filter: contrast(0);
 }
-svg {
+
+.slider_arrow_icon_left svg {
     width: 32px;
     height: 28px;
     flex-shrink: 0;
 }
-svg,
-svg g, svg g path {
-    /* mix-blend-mode: difference; */
-    stroke-width: 2px;
-    stroke: var(--Colors-text-primary, #2C2C2C) !important;
-    fill: var(--Colors-text-primary, #2C2C2C) !important;
-}
+
+/* .slider_arrow_icon svg_left g { */
+    /* mix-blend-mode: difference;     */
+    /* filter: saturate(1); */
+/* } */
+/* .slider_arrow_icon svg g path { */
+    /* stroke: var(--Colors-text-primary, #2C2C2C) !important; */
+    /* fill: var(--Colors-text-primary, #2C2C2C) !important; */
+/* } */
 
 </style>
--- components/icons/SliderArrowRight.vue
+++ components/icons/SliderArrowRight.vue
@@ -1,35 +1,41 @@
 <template>
-    <div class="icon">
+    <div class="slider_arrow_icon_right">
     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="28" viewBox="0 0 32 28" fill="none">
-        <!-- <g style="mix-blend-mode:difference"> -->
         <g>
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M17.5839 28L31.9999 14.7471L31.9999 13.253L17.5838 1.1074e-05L16.1825 1.49412L28.6981 13L1.31134e-06 13L1.3131e-06 15L28.6982 15L16.1825 26.5059L17.5839 28Z" fill="white"/>
+            <path fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M17.5839 28L31.9999 14.7471L31.9999 13.253L17.5838 1.1074e-05L16.1825 1.49412L28.6981 13L1.31134e-06 13L1.3131e-06 15L28.6982 15L16.1825 26.5059L17.5839 28Z"
+                fill="var(--Colors-text-primary, #2C2C2C)"/>
         </g>
     </svg>
     </div>
 </template>
 <style scoped>
-.icon {
+.slider_arrow_icon_right {
     display: flex;
-width: var(--dimension-icon-size-L, 32px);
-height: var(--dimension-icon-size-L, 32px);
-padding: var(--margin-navbar-institution-logo-right, 10px);
-flex-direction: column;
-justify-content: center;
-align-items: center;
-gap: var(--margin-navbar-institution-logo-right, 10px);
+
+    width: var(--dimension-icon-size-L, 32px);
+    height: var(--dimension-icon-size-L, 32px);
+    padding: var(--margin-navbar-institution-logo-right, 10px);
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: var(--margin-navbar-institution-logo-right, 10px);
+    filter: contrast(0);
 }
-svg {
+
+.slider_arrow_icon_right svg {
     width: 32px;
     height: 28px;
     flex-shrink: 0;
 }
-svg,
-svg g, svg g path {
-    /* mix-blend-mode: difference; */
-    stroke-width: 2px;
-    stroke: var(--Colors-text-primary, #2C2C2C) !important;
-    fill: var(--Colors-text-primary, #2C2C2C) !important;
-}
+/* .slider_arrow_icon_right svg g {
 
+} */
+
+/* .slider_arrow_icon_right svg g path { */
+    /* mix-blend-mode: difference; */
+    /* stroke: var(--Colors-text-primary, #2C2C2C) !important; */
+    /* fill: var(--Colors-text-primary, #2C2C2C) !important; */
+/* } */
 </style>
