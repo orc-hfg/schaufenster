74ded76b37bbb15cbbb633d8a266b37513d19981
Alex Liebrich <alexanderliebrich@web.de>
2025-03-25 09:12:49 +0100

    dev deploy script

--- do-dist-dev.sh
+++ do-dist-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export NUXT_APP_BASE_URL=/schaufenster/; nuxi build
-rsync -avz .output alex@dev.madek.hfg-karlsruhe.de:/srv/schaufenster/nuxt-schaufenster/
-ssh alex@dev.madek.hfg-karlsruhe.de "sudo systemctl restart madek-schaufenster"
+rsync -avz .output alex@dev.madek.hfg-karlsruhe.de:/srv/dev/schaufenster/nuxt-schaufenster/
+ssh alex@dev.madek.hfg-karlsruhe.de "sudo systemctl restart madek.dev.schaufenster"
