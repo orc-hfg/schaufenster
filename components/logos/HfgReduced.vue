57442a89194ddc8c62adbf3b0c7ed37236276354
vfc <98529522+victor-fancelli-capdevila@users.noreply.github.com>
2025-05-14 13:57:46 +0200

    FIX Alt-Text von Logos in Infoseite #105

--- components/logos/HfgReduced.vue
+++ components/logos/HfgReduced.vue
@@ -1,3 +1,5 @@
 <template>
-<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2" viewBox="0 0 222 80"><path d="m213.516 0-29.027 59.297h8.504L221.743.018V0h-8.227ZM152.01 0l-29.009 59.297h8.936L160.688 0h-8.677Zm-92.26 0L30.74 59.297h8.937L68.446 0H59.75ZM29.011 0 0 59.297h8.94L37.69 0h-8.678Zm82.563 19.814-29.02 59.288H91.5l28.757-59.288h-8.681Zm61.494 0-29.02 59.288h8.937l28.76-59.288h-8.677Zm-92.254 0L51.806 79.102h8.937l28.76-59.288h-8.688Z"/></svg>
+<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2" viewBox="0 0 222 80">
+    <title>{{ $t('intro.title3')}} {{ $t('intro.title4')}} {{ $t('intro.title5')}}</title>
+    <path d="m213.516 0-29.027 59.297h8.504L221.743.018V0h-8.227ZM152.01 0l-29.009 59.297h8.936L160.688 0h-8.677Zm-92.26 0L30.74 59.297h8.937L68.446 0H59.75ZM29.011 0 0 59.297h8.94L37.69 0h-8.678Zm82.563 19.814-29.02 59.288H91.5l28.757-59.288h-8.681Zm61.494 0-29.02 59.288h8.937l28.76-59.288h-8.677Zm-92.254 0L51.806 79.102h8.937l28.76-59.288h-8.688Z"/></svg>
 </template>
