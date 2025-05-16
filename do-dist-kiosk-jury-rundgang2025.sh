afa8650b55a15f0f4129028e7b8ff6f061fd5298
Alex Liebrich <alexanderliebrich@web.de>
2025-05-16 11:45:26 +0200

    kiosk deploy scripts

--- do-dist-kiosk-jury-rundgang2025.sh
+++ do-dist-kiosk-jury-rundgang2025.sh
@@ -3,4 +3,4 @@ export NUXT_APP_KIOSK_SET_ID=d4130cb5-0101-463e-bf92-e9c1776778f7
 export NUXT_APP_BASE_URL=/juryrundgang2025
 npm run build
 rsync -avz .output alex@dev.madek.hfg-karlsruhe.de:/srv/staging/juryrundgang2025/
-ssh alex@dev.madek.hfg-karlsruhe.de "sudo systemctl restart madek.staging.juryrundgang2025"
+ssh alex@dev.madek.hfg-karlsruhe.de "sudo systemctl restart madek.staging.kiosk.juryrundgang2025"
--- do-dist-kiosk-rundgang2025.sh
+++ do-dist-kiosk-rundgang2025.sh
@@ -3,4 +3,4 @@ export NUXT_APP_KIOSK_SET_ID=13ab489e-5051-42ef-b55b-b5051b6031df
 export NUXT_APP_BASE_URL=/rundgang2025
 npm run build
 rsync -avz .output alex@dev.madek.hfg-karlsruhe.de:/srv/staging/rundgang2025/
-ssh alex@dev.madek.hfg-karlsruhe.de "sudo systemctl restart madek.staging.rundgang2025"
+ssh alex@dev.madek.hfg-karlsruhe.de "sudo systemctl restart madek.staging.kiosk.rundgang2025"
