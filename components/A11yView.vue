c939936e2c6d528ba4120f0e8e074c4b2e63494d
vfc <98529522+victor-fancelli-capdevila@users.noreply.github.com>
2025-07-04 16:38:26 +0200

    #204

--- components/A11yView.vue
+++ components/A11yView.vue
@@ -8,7 +8,8 @@
                 <NuxtLink 
                     tabindex="0"
                     @keyup.enter="doClose()"
-                    @click="doClose()" class="nav_close">
+                    @click="doClose()"
+                    class="nav_close">
                     <IconsNavHome/>
                 </NuxtLink>
             </nav>
@@ -16,15 +17,13 @@
                 <div class="wrapper_dropdown">
                     <div class="hc_selector"
                     tabindex="0"
+                    @keyup.enter="changeContrast()"
                     @click="changeContrast()"
                     >
-                    <div class="hc_label">
-                        {{ $t('static_pages.a11y.label') }}
+                     <IconsHighContrast/>
                     </div>
-                </div>
-            </div >
-            
-        </div>    
+                </div > 
+            </div>    
         </header>
 
         <div class="page_content">
@@ -148,7 +147,7 @@ onMounted(() => {
     /* border: 1px solid green; */
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: right;
     gap: var(--margin-navbar-institution-logo-right, 10px);
 }
 [data-layout="mobile"] {
