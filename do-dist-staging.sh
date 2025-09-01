e8d64eaf4845a8b42fa7ffe560b999bc2045b322
Hannes Herold <46819942+hannesherold@users.noreply.github.com>
2025-09-01 09:22:15 +0200

    Rename do-dist.sh, add $SUSER

--- do-dist-staging.sh
+++ do-dist-staging.sh
@@ -6,6 +6,9 @@ export NUXT_APP_BASE_URL='/schaufenster'
 #export NUXT_APP_USE_CACHED_DATA=1
 #export NUXT_APP_USE_CACHE_FILE='/home/madek/sf_dev_tree_data.json'
 
+# export SUSER=alex
+export SUSER=hherold
+
 npm run build
-rsync -avz .output alex@dev.madek.hfg-karlsruhe.de:/srv/staging/schaufenster/
-ssh alex@dev.madek.hfg-karlsruhe.de "sudo systemctl restart madek.staging.schaufenster"
+rsync -avz .output $SUSER@dev.madek.hfg-karlsruhe.de:/srv/staging/schaufenster/
+ssh $SUSER@dev.madek.hfg-karlsruhe.de "sudo systemctl restart madek.staging.schaufenster"
