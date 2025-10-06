a49c8a60320ac9e02c79c4b677d0978c8d6e1159
Alex Liebrich <alexanderliebrich@web.de>
2025-10-06 23:46:27 +0200

    use about view

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,4 @@
 <template>
-    <div>
-      <h1>About Schaufenster</h1>
-      <AppAlert>
-        This is an auto-imported component
-      </AppAlert>
-    </div>
-  </template>
+  <AboutView/>
+</template>
   
