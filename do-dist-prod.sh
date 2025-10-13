310132b2925e78cbfebdec0db6eb619e8e8ec975
Alex Liebrich <alexanderliebrich@web.de>
2025-10-13 09:07:32 +0200

    deploy prod without pw

--- do-dist-prod.sh
+++ do-dist-prod.sh
@@ -3,7 +3,7 @@ export NUXT_APP_ROOT_SET_ID='ec8d67e0-e27b-44dc-8e10-3b6dc2cc340e'
 export NUXT_APP_API_BASE_URL='https://madek.hfg-karlsruhe.de'
 export API_BASE_URL='https://madek.hfg-karlsruhe.de'
 export NUXT_APP_BASE_URL='/'
-export PASSWORD_PROTECT='SF2025'
+#export PASSWORD_PROTECT='SF2025'
 export NUXT_APP_USE_CACHED_DATA=1
 export NUXT_APP_USE_CACHE_FILE='/home/madek/schaufenster_tree_data.json'
 
